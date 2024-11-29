247da16f9f9eee0f9b44517b34a5778db256a8f5
Leo Omnes <l.omnes@lprs.fr>
2024-11-29 11:39:18 +0100

    fix(connexion): reglage de la verif du mdp / email

--- connexion.php
+++ connexion.php
@@ -3,14 +3,16 @@ var_dump($_POST);
 if (isset($_POST['emailCo']) && isset($_POST['mdpCo'])) {
         $bdd = new PDO('mysql:host=localhost;dbname=gestionlivre;charset=utf8', 'root', '');
         $req = $bdd->prepare('SELECT * FROM inscrit WHERE email = :email AND mdp = :mdp');
-        $info=$req->execute(array(
+        $req->execute(array(
             'email' => $_POST['emailCo'],
             'mdp' => $_POST['mdpCo']));
-        $utilisateur = $info->fetch();
+        $utilisateur = $req->fetch();
         if ($utilisateur) {
+            var_dump($utilisateur);
             echo "Connexion reussit ! ";
         }
         else {
+            
             echo "Mdp ou email incorrecte ! ";
         }
 }
