763f87387e038bd98f5032cdaa71279b2766652b
Leo Omnes <l.omnes@lprs.fr>
2024-12-11 09:37:49 +0100

    fix gestion admin supression dans affichage donnee

--- affichage_donnee.php
+++ affichage_donnee.php
@@ -5,15 +5,9 @@
 <body>
 <?php
 session_start();
-if($_SESSION['email']=="admin@admin" && $_SESSION['mdp']=="admin"){
-    echo "vous etes l'admin";
-   header ("location : admin.php");
-}
-else{
     echo "Bienvenue sur votre session ". $_SESSION['nom']." ".$_SESSION['prenom']."<br>";
     echo "Vous habitez au ".$_SESSION['rue']." ".$_SESSION['ville'].", ".$_SESSION['cp']."<br>";
     header ("location : utilisateur.php");
-}
 ?>
 
 
